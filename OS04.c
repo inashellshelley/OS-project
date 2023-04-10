798769c5d366bcd1ad9b95093e30b7fb8f2a8824
Shalini <91945116+inashellshelley@users.noreply.github.com>
2023-04-10 21:21:28 +0530

    Update OS04.c

--- OS04.c
+++ OS04.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <stdbool.h>
 #include <time.h>
+#include <stdbool.h>
+#include <pthread.h>
 
 int numResources,
     numProcesses;
